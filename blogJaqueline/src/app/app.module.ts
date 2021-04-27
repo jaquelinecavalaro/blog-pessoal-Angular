27fb805a28e1b5ffb2388798a05a79a916d52032
Jaquelinecavalaro <jaqueline.cavalaro@gmail.com>
2021-04-27 19:55:08 -0300

    Second up enter and create acc

--- blogJaqueline/src/app/app.module.ts
+++ blogJaqueline/src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
-import { ContatoComponent } from './contato/contato.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     RodapeComponent,
-    ContatoComponent
+    EntrarComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule,
