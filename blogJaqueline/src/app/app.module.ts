707fc141ea2bfd0603be527edc882cff7225547c
Jaquelinecavalaro <jaqueline.cavalaro@gmail.com>
2021-04-27 14:10:33 -0300

    First implementation menu and footer

--- blogJaqueline/src/app/app.module.ts
+++ blogJaqueline/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
