2bab047335ee7faceb7c83f7194d4bf17b60f5cf
Jaquelinecavalaro <jaqueline.cavalaro@gmail.com>
2021-04-30 09:17:31 -0300

    att rocket

--- blogJaqueline/src/app/rocket/rocket.component.ts
+++ blogJaqueline/src/app/rocket/rocket.component.ts
@@ -9,7 +9,38 @@ export class RocketComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.stars()
   }
 
+  stars() 
+  {
+      let count = 50;
+      let scene = document.querySelector('.scene');
+      let i = 0;
+
+      while (i < count) 
+      {
+          let star = document.createElement('i');
+          let x = Math.floor(Math.random() * window.innerWidth);
+
+          let duration = Math.random() * 1;
+          let h = Math.random() * 100;
+
+          star.style.left = x + 'px';
+          star.style.width = 1 + 'px';
+          star.style.height = 50 + h + 'px';
+          star.style.animationDuration = duration + 's';
+
+          if (scene != null){
+            scene.appendChild(star)
+            }
+          i++;
+      }
+  }
 }
+
+    
+    
+    
+
